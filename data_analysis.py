d0d80c75467ece1e180778239a1c344238ad73b3
clarajean16 <154241550+clarajean16@users.noreply.github.com>
2024-06-03 13:44:58 +0200

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -19,16 +19,17 @@ dev_texts = dev_data['text']
 
 # 1. size of train dataset
 num_instances = train_data.shape[0]
-print("Number of data instances in train:", num_instances)
+print("1. size of dataset: \nNumber of data instances in train:", num_instances)
 num_instances = test_data.shape[0]
 print("Number of data instances in testing data:", num_instances)
 num_instances = dev_data.shape[0]
-print("Number of data instances in validation data:", num_instances)
+print("Number of data instances in validation data:", num_instances, '\n')
 
 # 2. balance of dataset, how many data instances are labeled plausible versus implausible
 label_counts = train_labels.value_counts()
-print("Label counts:\n", label_counts)
-plt.figure(figsize=(8, 6))
+label_counts_dict = label_counts.to_dict()
+print("2. balance of dataset, how many data instances are labeled plausible versus implausible \nLabel counts:\n", label_counts_dict, '\n')
+plt.figure(figsize=(8, 6)) # makes plot
 label_counts.plot(kind='bar', color=['blue', 'orange'])
 plt.title('Counts of Each Label')
 plt.xlabel('Label')
@@ -36,5 +37,16 @@ plt.ylabel('Count')
 plt.xticks(rotation=0)
 plt.show()
 
-# 3. number of items in each data instance
 
+# 3. number of items in each data instance
+word_counts = train_texts.apply(lambda x: len(x.split()))
+word_length_counts = word_counts.value_counts().sort_index()
+word_length_dict = word_length_counts.to_dict()
+print("3. number of items in each data instance:\n", word_length_dict)
+plt.figure(figsize=(10, 6)) # makes bar chart
+word_length_counts.plot(kind='bar', color='skyblue')
+plt.title('Distribution of Word Counts in Text Instances')
+plt.xlabel('Number of Words')
+plt.ylabel('Number of Instances')
+plt.xticks(rotation=0)
+plt.show()
