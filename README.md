d0e121d08d56075911f703fd87c5e7d3d3c0e640
ukhovs <ukhovsergeyy@gmail.com>
2024-06-05 13:35:36 +0200

    Still need to finish point 7

--- README.md
+++ README.md
@@ -17,30 +17,67 @@ Basic features of data:
 Linguistic Characteristics:
 
 6. Part-of-Speech tag distribution SHAWN
-   - intuition: though the distribution of POS tags in the dataset are mostly subject (Noun), verb, object (Noun), we found       some words are semantically ambiguous (e.g., the word chill can be both a noun, adjective, and verb); thus these words       may affect the model's performance
-   - we used two different POS taggers: average perceptron tagger from NLTK (1/6 of the tokens are tagged as JJ) and       
+   - intuition: though the distribution of POS tags in the dataset are mostly subject (Noun), verb, object (Noun), we found some words are semantically ambiguous (e.g., the word chill can be both a noun, adjective, and verb); thus these words may affect the model's performance
+   - we used two different POS taggers: average perceptron tagger from NLTK (1/6 of the tokens are tagged as JJ) and  
      en_core_web_sm from spacy; and we chose to keep the results from spacy
    - library/packages used: pandas, matplotlib, spacy
-   
-7. how many times words appear in the dataset as subject or object and their corresponding labels SERGEI
-8. (Dis)agreement in annotation, Kohen's kappa:
-   each annotate selection of dataset and see if we agree with the labeled annotations +
-9. running a kNN cluster against the dataset SERGEI
+7. Number of times words appear in the dataset as both subject and object (S-O), subject and verb (S-V), verb and object (V-O). SERGEI
+
+   S-O Train: 2143
+   S-O Dev: 208
+   S-O Test: 228
+
+   S-V Train:
+   S-V Dev:
+   S-V Test:
+
+   V-O Train:
+   V-O Dev:
+   V-O Test:
+
+8. (Dis)agreement in annotation, Cohen's kappa. SERGEI
+   We have randomly selected 20 instances and each of us annotated them to see if we agree with the labeled annotations. Cohen's kappa (imported from sklearn.metrics) was used for the calculation of the agreement in annotation. The results are as follows:
+   Clara vs Gold kappa score: 0.9
+   Sergei vs Gold kappa score: 0.61
+   Hao-En vs Gold kappa score: 1.0
+   Average kappa score: 0.83 (strong agreement)
+
+9. Out-of-vocabulary (OOV) words in dev and test data. SERGEI
+   Number of OOV words in dev set: 13
+   Dev set OOV percentage: 0.039
+   Number of OOV words in test set: 12
+   Test set OOV percentage: 0.038
 
 10. Relationships across data splits: CLARA
     compare basic features of data across test, train, and dev
 
 11. word concreteness (wordnet, NLTK) CLARA
 12. co-occurrence/collocation (compared to large corpus) SHAWN
-    - intuition: if some n-grams (bigram and trigram here) occur frequently in a corpus, it is much more likely to be              plausible
+    - intuition: if some n-grams (bigram and trigram here) occur frequently in a corpus, it is much more likely to be plausible
     - corpus used: brown (W. N. Francis and H. Kucera [1964]) from NLTK (http://www.hit.uib.no/icame/brown/bcm.html)
     - library/packages used: pandas, nltk, matplotlib
-      
-13. action affordance (spaCy) SERGEI
+13. Distribution of gender-specific nouns in the dataset. SERGEI
+
+    We have created three lists of gender-specific nouns (masculine, feminine, and neutral) that are common in English and appear in the dataset to see if the distribution is balanced. Nouns relating to human beings only were considered.
+
+    Train set:
+    Number of masculine nouns: 273, Percentage among gender-specific nouns: 0.35
+    Number of feminine nouns: 199, Percentage among gender-specific nouns: 0.25
+    Number of neutral nouns: 319, Percentage among gender-specific nouns: 0.40
+
+    Dev set:
+    Number of masculine nouns: 33, Percentage among gender-specific nouns: 0.31
+    Number of feminine nouns: 29, Percentage among gender-specific nouns: 0.27
+    Number of neutral nouns: 44, Percentage among gender-specific nouns: 0.42
+
+    Test set:
+    Number of masculine nouns: 38, Percentage among gender-specific nouns: 0.32
+    Number of feminine nouns: 30, Percentage among gender-specific nouns: 0.25
+    Number of neutral nouns: 52, Percentage among gender-specific nouns: 0.43
+
 14. commonsense knowledge (python package, conceptnet) CLARA
 15. n-grams SHAWN
-    
 16. relation between subjects being animate/inanimate and their labels SHAWN
     - intuition: our hypothesis is that it is more likely that the sentences wtih animate subjects are plausible
-    - dataset used: en-animacy-train (lingvenvist/en-animacy), link: https://huggingface.co/datasets/lingvenvist/en-animacy/tree/main; after that we trimmed the dataset and keep only animate nouns (the entries labelled as "H" as human       and "A" as animate) and saved into another file called animate_nouns.txt
+    - dataset used: en-animacy-train (lingvenvist/en-animacy), link: https://huggingface.co/datasets/lingvenvist/en-animacy/tree/main; after that we trimmed the dataset and keep only animate nouns (the entries labelled as "H" as human and "A" as animate) and saved into another file called animate_nouns.txt
     - library/packages used: pandas, matplotlib
--- kappa.py
+++ kappa.py
@@ -1,7 +1,11 @@
 from sklearn.metrics import cohen_kappa_score
 import pandas as pd
 
+# reading from train, dev, test
 train_data = pd.read_csv('train.csv')
+test_data = pd.read_csv('test.csv')
+dev_data = pd.read_csv('dev.csv')
+
 # Kappa score for the train data
 annotation_data = pd.read_csv("annotation agreement.csv")
 # print(annotation_data)
@@ -16,68 +20,70 @@ sergei_gold = (cohen_kappa_score(sergei_anot,gold_standard))
 shawn_gold = (cohen_kappa_score(shawn_anot,gold_standard))
 
 aver_kappa_score = (clara_gold + sergei_gold + shawn_gold)/3
+# print(clara_gold)
+# print(sergei_gold)
+# print(shawn_gold)
 # print(aver_kappa_score)
 
-# Total number of all words in train data
+# Total number of all words 
 
-all_words_train = list()
-for i in train_data['text']:
-    i = i.split()
-    for j in i:
-        all_words_train.append(j)
+def all_words(pd_file):
+    list_of_all_words = list()
+    for i in pd_file['text']:
+        i = i.split()
+        for j in i:
+            list_of_all_words.append(j)
+    return list_of_all_words
 
-# Train vocabulary
+# Function extracting a set of unique words from a file
 
-vocabulary_train = set()
-for i in train_data['text']:
-    i = i.split()
-    for j in i: 
-        vocabulary_train.add(j)
-# print(vocabulary)
-
-# Writing all unique words from train data into a new file to check for animacy
-
-# animacy_file = 'animacy.txt'
-
-
-# with open(animacy_file, 'w') as file:
-#     for unique_word in vocabulary_train:
-#         file.write(unique_word + '\n')
+def vocabulary(pd_file):
+    unique_words = set()
+    for i in pd_file['text']:
+        i = i.split()
+        for j in i: 
+           unique_words.add(j)
+    return unique_words
 
 
+# 7. Number of times a noun is both subject and object
 
-# Number of times a word is both S and O
+def subject_and_object(pd_file):
+    instances = pd_file['text'] 
+    subject = []
+    verb = [] 
+    object = []
+    for instance in instances:
+        instance = instance.split()
+        subject.append(instance[0])
+        verb.append(instance[1])
+        object.append(instance[2])
 
-instances = train_data['text'] 
-subject = []
-verb = [] 
-object = []
-for instance in instances:
-    instance = instance.split()
-    subject.append(instance[0])
-    verb.append(instance[1])
-    object.append(instance[2])
+    subject_object = []
+    for i in subject:
+        if i in object:
+            subject_object.append(i)
+    return len(subject_object)
 
-subject_object = []
-for i in subject:
-    if i in object:
-        subject_object.append(i)
-# print(subject_object)
+print("Number of nouns in train set that are both subject and object:",subject_and_object(train_data))
+print("Number of nouns in dev set that are both subject and object:",subject_and_object(dev_data))
+print("Number of nouns in test set that are both subject and object:",subject_and_object(test_data))
 
-''' Distribution of gender-specific nouns in the dataset.
+
+''' 13. Distribution of gender-specific nouns in the dataset.
 Checked for those among animals, e.g. cow/bull, duck/drake, mare/stallion, but the dataset does not have those'''
 
-# List of gendered nouns to check against
-masculine_nouns = ['man','father','boy','husband','uncle','actor','policeman','dad']
-feminine_nouns = ['woman','mother','girl','wife','aunt','actress','witch','girlfriend','grandma','women']
-gender_neutral_nouns = ['person','parent','child','kid','baby','spouse','infant','people']
+#List of gendered nouns to check against
+masculine_nouns = ['man','father','boy','husband','uncle','policeman','dad','superman','son']
+feminine_nouns = ['woman','mother','girl','wife','aunt','witch','girlfriend','grandma','women','daughter']
+gender_neutral_nouns = ['person','parent','child','kid','baby','spouse','infant','people','human','student','chef','clown','baker','doctor','dentist','boxer','chef','officer','barber','wrestler']
 
-# Counting the number of occurences of M, F, and GN nouns
+# Counting the number of occurences of masculine, feminine, and neutral nouns
 masculine_count = 0
 feminine_count = 0
 gender_neutral_count = 0
 
-for i in all_words_train:
+for i in all_words(test_data):
     if i in masculine_nouns:
         masculine_count += 1
     elif i in feminine_nouns:
@@ -88,7 +94,31 @@ for i in all_words_train:
 # Overall number of gendered nouns. To be used for normalisation
 total_sum = masculine_count + feminine_count + gender_neutral_count
 
-# print(all_words)
-# print("Masculine nouns:", masculine_count, ",Percentage:", masculine_count/total_sum)
-# print("Feminine nouns:", feminine_count, ",Percentage:", feminine_count/total_sum)
-# print("Gender neutral nouns:", gender_neutral_count, ",Percentage:", gender_neutral_count/total_sum)
+# print("Number of masculine nouns:", masculine_count, ", Percentage among gender-specific nouns:", masculine_count/total_sum)
+# print("Number of feminine nouns:", feminine_count, ", Percentage among gender-specific nouns:", feminine_count/total_sum)
+# print("Number of neutral nouns:", gender_neutral_count, ", Percentage among gender-specific nouns:", gender_neutral_count/total_sum)
+
+
+# 9. Out-of-vocabulary words in dev and test
+
+vocabulary_train = vocabulary(train_data)
+vocabulary_dev = vocabulary(dev_data)
+vocabulary_test = vocabulary(test_data)
+
+def out_of_vocabulary(vocabulary):
+    out_of_vocab = set()
+    for word in vocabulary:
+        if word not in vocabulary_train:
+            out_of_vocab.add(word)
+    return out_of_vocab
+
+oov_dev = out_of_vocabulary(vocabulary_dev)
+oov_test = out_of_vocabulary(vocabulary_test)
+
+# print(oov_dev) #Words in dev set that do not appear in train set
+# print("Number of OOV words in dev set:",len(oov_dev), "\nDev OOV Percentage:",len(oov_dev)/len(vocabulary_dev)) #Output: 13, 0.039
+# print(oov_test) #Words in test set that do not appear in train set
+# print("Number of OOV words in test set:",len(oov_test), "\nTest OOV Percentage:",len(oov_test)/len(vocabulary_test)) #Output: 12, 0.038
+
+# print(len(vocabulary_dev))
+# print(len(vocabulary_test))
