e060504b3bb2b75222784d6a5247354bb97b44fb
ukhovs <ukhovsergeyy@gmail.com>
2024-06-22 11:51:46 +0200

    KNN + RF

--- knn.py
+++ knn.py
@@ -9,6 +9,8 @@ from sklearn.neighbors import KNeighborsClassifier
 # from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import log_loss
 
 def get_sentence_embeddings(text_list, batch_size=32):
     all_embeddings = []
@@ -34,7 +36,6 @@ def get_sentence_embeddings(text_list, batch_size=32):
 def get_strings(dataframe):
     strings_for_bert = []
     for index, sentence in enumerate(dataframe['text']):
-        emotion_label = dataframe['label'][index]
         strings_for_bert.append (sentence)
     return strings_for_bert
 
@@ -67,89 +68,115 @@ train_sentence_embeddings = get_sentence_embeddings(train_strings)
 # # Get test embeddings
 test_sentence_embeddings = get_sentence_embeddings(test_strings)
 
-def load_data(file_path):
-    data = pd.read_csv(file_path, header=None)
-    texts = data[1].tolist()
-    features = data[[2, 3]].values
-    labels = data[0].values
-    return texts, features, labels
-
-train_texts, train_features, train_labels = load_data('train_labeled.csv')
-test_texts, test_features, test_labels = load_data('test_labeled.csv')
-
-
-new_train_features = []
-for i in train_features:
-    new_train_features.append([sum(i)])
-# print(new_train_features)
-
-# very_new_train_features = []
-# for i in new_train_features:
-#     if i[0] == 0:
-#         very_new_train_features.append([i[0]])
-#     else:
-#         very_new_train_features.append([i[0]/2])
-# # print(very_new_train_features)
-
-new_test_features = []
-for i in test_features:
-    new_test_features.append([sum(i)])
-# print(new_test_features)
-
-# very_new_test_features = []
-# for i in new_test_features:
-#     if i[0] == 0:
-#         very_new_test_features.append([i[0]])
-#     else:
-#         very_new_test_features.append([i[0]/2])
-# # print(very_new_test_features)
-
-
-# Adding animacy features to BERT embeddings
-train_features_combined = np.concatenate((train_sentence_embeddings, new_train_features),axis=1)
-test_features_combined = np.concatenate((test_sentence_embeddings, new_test_features), axis=1)
-
-# print(test_features_combined)
-
-
-# KNN Classifier 
-knn1 = KNeighborsClassifier(n_neighbors=1,weights='uniform',)
-knn5 = KNeighborsClassifier(n_neighbors=5)
-knn7 = KNeighborsClassifier(n_neighbors=7)
-knn9 = KNeighborsClassifier(n_neighbors=9)
-
-# X_train = train_sentence_embeddings
-X_train = train_features_combined
+X_train = train_sentence_embeddings
 y_train = list(train_data['label'])
 
-# X_test = test_sentence_embeddings
-X_test = test_features_combined
+X_test = test_sentence_embeddings
 y_test = list(test_data['label'])
 
-# Train KNN
+rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_classifier.fit(X_train, y_train)
 
-knn1.fit(X_train, y_train)
-knn5.fit(X_train, y_train)
-knn7.fit(X_train, y_train)
-knn9.fit(X_train, y_train)
+# Predict on the test set
+y_pred = rf_classifier.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy}")
+
+y_prob = rf_classifier.predict_proba(X_test)
+log_loss_value = log_loss(y_test, y_prob)
+
+print(log_loss_value)
+
+# def load_data(file_path):
+#     data = pd.read_csv(file_path, header=None)
+#     texts = data[1].tolist()
+#     features = data[[2, 3]].values
+#     labels = data[0].values
+#     return texts, features, labels
+
+# train_texts, train_features, train_labels = load_data('train_labeled.csv')
+# test_texts, test_features, test_labels = load_data('test_labeled.csv')
 
 
-# Predict 
-y_pred_1 = knn1.predict(X_test)
-y_pred_5 = knn5.predict(X_test)
-y_pred_7 = knn7.predict(X_test)
-y_pred_9 = knn9.predict(X_test)
+# new_train_features = []
+# for i in train_features:
+#     new_train_features.append([sum(i)])
+# # print(new_train_features)
+
+# # very_new_train_features = []
+# # for i in new_train_features:
+# #     if i[0] == 0:
+# #         very_new_train_features.append([i[0]])
+# #     else:
+# #         very_new_train_features.append([i[0]/2])
+# # # print(very_new_train_features)
+
+# new_test_features = []
+# for i in test_features:
+#     new_test_features.append([sum(i)])
+# # print(new_test_features)
+
+# # very_new_test_features = []
+# # for i in new_test_features:
+# #     if i[0] == 0:
+# #         very_new_test_features.append([i[0]])
+# #     else:
+# #         very_new_test_features.append([i[0]/2])
+# # # print(very_new_test_features)
 
 
-# Print accuracy
-print("Accuracy with k=1", accuracy_score(y_test, y_pred_1))
-print("Accuracy with k=5", accuracy_score(y_test, y_pred_5))
-print("Accuracy with k=7", accuracy_score(y_test, y_pred_7))
-print("Accuracy with k=9", accuracy_score(y_test, y_pred_9))
+# # Adding animacy features to BERT embeddings
+# train_features_combined = np.concatenate((train_sentence_embeddings, new_train_features),axis=1)
+# test_features_combined = np.concatenate((test_sentence_embeddings, new_test_features), axis=1)
+
+# # print(test_features_combined)
 
 
-# Print F1 score
-print("F1-Score with k=1", f1_score(y_test, y_pred_1))
-print("F1-Score with k=5", f1_score(y_test, y_pred_5))
-print("F1-Score with k=7", f1_score(y_test, y_pred_7))
-print("F1-Score with k=9", f1_score(y_test, y_pred_9))
+# # KNN Classifier 
+# knn1 = KNeighborsClassifier(n_neighbors=1,metric='chebyshev')
+# knn3 = KNeighborsClassifier(n_neighbors=3,metric='chebyshev')
+# knn5 = KNeighborsClassifier(n_neighbors=5,metric='chebyshev')
+# knn7 = KNeighborsClassifier(n_neighbors=7,metric='chebyshev')
+# knn9 = KNeighborsClassifier(n_neighbors=9,metric='chebyshev')
+
+# # X_train = train_sentence_embeddings
+# X_train = train_features_combined
+# y_train = list(train_data['label'])
+
+# # X_test = test_sentence_embeddings
+# X_test = test_features_combined
+# y_test = list(test_data['label'])
+
+# # Train KNN
+
+# knn1.fit(X_train, y_train)
+# knn3.fit(X_train, y_train)
+# knn5.fit(X_train, y_train)
+# knn7.fit(X_train, y_train)
+# knn9.fit(X_train, y_train)
+
+
+# # Predict 
+# y_pred_1 = knn1.predict(X_test)
+# y_pred_3 = knn3.predict(X_test)
+# y_pred_5 = knn5.predict(X_test)
+# y_pred_7 = knn7.predict(X_test)
+# y_pred_9 = knn9.predict(X_test)
+
+
+# # Print accuracy
+# print("Accuracy with k=1", accuracy_score(y_test, y_pred_1))
+# print("Accuracy with k=3", accuracy_score(y_test, y_pred_3))
+# print("Accuracy with k=5", accuracy_score(y_test, y_pred_5))
+# print("Accuracy with k=7", accuracy_score(y_test, y_pred_7))
+# print("Accuracy with k=9", accuracy_score(y_test, y_pred_9))
+ 
+
+# # Print F1 score
+# print("F1-Score with k=1", f1_score(y_test, y_pred_1))
+# print("F1-Score with k=1", f1_score(y_test, y_pred_3))
+# print("F1-Score with k=5", f1_score(y_test, y_pred_5))
+# print("F1-Score with k=7", f1_score(y_test, y_pred_7))
+# print("F1-Score with k=9", f1_score(y_test, y_pred_9))
