b2850c5648ded9a7ff20c1c69d4a869cd201e06c
ukhovs <ukhovsergeyy@gmail.com>
2024-07-04 12:43:28 +0200

    Data from 20Q added, TSV to CSV converted

--- knn.py
+++ knn.py
@@ -9,8 +9,7 @@ from sklearn.neighbors import KNeighborsClassifier
 # from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import log_loss
+from sklearn.metrics import roc_auc_score, roc_curve, auc
 
 def get_sentence_embeddings(text_list, batch_size=32):
     all_embeddings = []
@@ -67,116 +66,182 @@ train_sentence_embeddings = get_sentence_embeddings(train_strings)
 
 # # Get test embeddings
 test_sentence_embeddings = get_sentence_embeddings(test_strings)
-
-X_train = train_sentence_embeddings
-y_train = list(train_data['label'])
-
-X_test = test_sentence_embeddings
-y_test = list(test_data['label'])
-
-rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
-rf_classifier.fit(X_train, y_train)
-
-# Predict on the test set
-y_pred = rf_classifier.predict(X_test)
-
-# Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy}")
-
-y_prob = rf_classifier.predict_proba(X_test)
-log_loss_value = log_loss(y_test, y_prob)
-
-print(log_loss_value)
-
-# def load_data(file_path):
-#     data = pd.read_csv(file_path, header=None)
-#     texts = data[1].tolist()
-#     features = data[[2, 3]].values
-#     labels = data[0].values
-#     return texts, features, labels
-
-# train_texts, train_features, train_labels = load_data('train_labeled.csv')
-# test_texts, test_features, test_labels = load_data('test_labeled.csv')
-
-
-# new_train_features = []
-# for i in train_features:
-#     new_train_features.append([sum(i)])
-# # print(new_train_features)
-
-# # very_new_train_features = []
-# # for i in new_train_features:
-# #     if i[0] == 0:
-# #         very_new_train_features.append([i[0]])
-# #     else:
-# #         very_new_train_features.append([i[0]/2])
-# # # print(very_new_train_features)
-
-# new_test_features = []
-# for i in test_features:
-#     new_test_features.append([sum(i)])
-# # print(new_test_features)
-
-# # very_new_test_features = []
-# # for i in new_test_features:
-# #     if i[0] == 0:
-# #         very_new_test_features.append([i[0]])
-# #     else:
-# #         very_new_test_features.append([i[0]/2])
-# # # print(very_new_test_features)
-
-
-# # Adding animacy features to BERT embeddings
-# train_features_combined = np.concatenate((train_sentence_embeddings, new_train_features),axis=1)
-# test_features_combined = np.concatenate((test_sentence_embeddings, new_test_features), axis=1)
-
-# # print(test_features_combined)
-
-
-# # KNN Classifier 
-# knn1 = KNeighborsClassifier(n_neighbors=1,metric='chebyshev')
-# knn3 = KNeighborsClassifier(n_neighbors=3,metric='chebyshev')
-# knn5 = KNeighborsClassifier(n_neighbors=5,metric='chebyshev')
-# knn7 = KNeighborsClassifier(n_neighbors=7,metric='chebyshev')
-# knn9 = KNeighborsClassifier(n_neighbors=9,metric='chebyshev')
-
-# # X_train = train_sentence_embeddings
-# X_train = train_features_combined
+dev_sentence_embeddings = get_sentence_embeddings(dev_strings)
+# X_train = train_sentence_embeddings
 # y_train = list(train_data['label'])
 
-# # X_test = test_sentence_embeddings
-# X_test = test_features_combined
+# X_test = test_sentence_embeddings
 # y_test = list(test_data['label'])
 
-# # Train KNN
+# rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+# rf_classifier.fit(X_train, y_train)
 
-# knn1.fit(X_train, y_train)
-# knn3.fit(X_train, y_train)
-# knn5.fit(X_train, y_train)
-# knn7.fit(X_train, y_train)
-# knn9.fit(X_train, y_train)
+# # Predict on the test set
+# y_pred = rf_classifier.predict(X_test)
 
-
-# # Predict 
-# y_pred_1 = knn1.predict(X_test)
-# y_pred_3 = knn3.predict(X_test)
-# y_pred_5 = knn5.predict(X_test)
-# y_pred_7 = knn7.predict(X_test)
-# y_pred_9 = knn9.predict(X_test)
+# # Evaluate the model
+# accuracy = accuracy_score(y_test, y_pred)
+# print(f"Accuracy: {accuracy}")
 
 
-# # Print accuracy
-# print("Accuracy with k=1", accuracy_score(y_test, y_pred_1))
-# print("Accuracy with k=3", accuracy_score(y_test, y_pred_3))
-# print("Accuracy with k=5", accuracy_score(y_test, y_pred_5))
-# print("Accuracy with k=7", accuracy_score(y_test, y_pred_7))
-# print("Accuracy with k=9", accuracy_score(y_test, y_pred_9))
+def load_data(file_path):
+    data = pd.read_csv(file_path, header=None)
+    texts = data[1].tolist()
+    features = data[[2, 3]].values
+    labels = data[0].values
+    return texts, features, labels
+
+train_texts, train_features, train_labels = load_data('train_labeled.csv')
+test_texts, test_features, test_labels = load_data('test_labeled.csv')
+dev_texts, dev_features, dev_labels = load_data('dev_labeled.csv')
+
+
+animacy_weight = 20
+new_train_features = []
+for i in train_features:
+    new_train_features.append([sum(i)*animacy_weight])
+    # new_train_features.append([sum(i)])
+# print(new_train_features)
+
+new_test_features = []
+for i in test_features:
+    new_test_features.append([sum(i)*animacy_weight])
+    # new_test_features.append([sum(i)])
+# print(new_test_features)
+
+new_dev_features = []
+for i in dev_features:
+    new_dev_features.append([sum(i)*animacy_weight])
+    # new_dev_features.append([sum(i)])
+
+
+
+# Adding animacy features to BERT embeddings
+train_features_combined = np.concatenate((train_sentence_embeddings, new_train_features),axis=1)
+test_features_combined = np.concatenate((test_sentence_embeddings, new_test_features), axis=1)
+dev_features_combined = np.concatenate((dev_sentence_embeddings, new_dev_features), axis=1)
+# print(test_features_combined)
+
+
+# KNN Classifier 
+knn1 = KNeighborsClassifier(n_neighbors=1,metric='manhattan')
+knn3 = KNeighborsClassifier(n_neighbors=3,metric='manhattan')
+knn5 = KNeighborsClassifier(n_neighbors=5,metric='manhattan')
+knn7 = KNeighborsClassifier(n_neighbors=7,metric='manhattan')
+knn9 = KNeighborsClassifier(n_neighbors=9,metric='manhattan')
+knn11 = KNeighborsClassifier(n_neighbors=11,metric='manhattan')
+
+# X_train = train_sentence_embeddings
+X_train = train_features_combined
+y_train = list(train_data['label'])
+
+# X_test = test_sentence_embeddings
+X_test = test_features_combined
+y_test = list(test_data['label'])
+
+# X_dev = dev_sentence_embeddings
+X_dev = dev_features_combined
+y_dev = list(dev_data['label'])
+
+# Train KNN
+
+knn1.fit(X_train, y_train)
+knn3.fit(X_train, y_train)
+knn5.fit(X_train, y_train)
+knn7.fit(X_train, y_train)
+knn9.fit(X_train, y_train)
+knn11.fit(X_train, y_train)
+
+
+# Predict 
+
+y_pred_dev_1 = knn1.predict(X_dev)
+y_pred_dev_3 = knn3.predict(X_dev)
+y_pred_dev_5 = knn5.predict(X_dev)
+y_pred_dev_7 = knn7.predict(X_dev)
+y_pred_dev_9 = knn9.predict(X_dev)
+y_pred_dev_11 = knn11.predict(X_dev)
+
+y_prob1_dev = knn1.predict_proba(X_dev)[:, 1]
+y_prob3_dev = knn3.predict_proba(X_dev)[:, 1]
+y_prob5_dev = knn5.predict_proba(X_dev)[:, 1]
+y_prob7_dev = knn7.predict_proba(X_dev)[:, 1]
+y_prob9_dev = knn9.predict_proba(X_dev)[:, 1]
+y_prob11_dev = knn11.predict_proba(X_dev)[:, 1]
+
+y_pred_test_1 = knn1.predict(X_test)
+y_pred_test_3 = knn3.predict(X_test)
+y_pred_test_5 = knn5.predict(X_test)
+y_pred_test_7 = knn7.predict(X_test)
+y_pred_test_9 = knn9.predict(X_test)
+y_pred_test_11 = knn11.predict(X_test)
+
+y_prob1_test = knn1.predict_proba(X_test)[:, 1]
+y_prob3_test = knn3.predict_proba(X_test)[:, 1]
+y_prob5_test = knn5.predict_proba(X_test)[:, 1]
+y_prob7_test = knn7.predict_proba(X_test)[:, 1]
+y_prob9_test = knn9.predict_proba(X_test)[:, 1]
+y_prob11_test = knn11.predict_proba(X_test)[:, 1]
+
+# Print accuracy dev
+# print("Accuracy DEV with k=1", accuracy_score(y_dev, y_pred_dev_1))
+# print("Accuracy DEV with k=3", accuracy_score(y_dev, y_pred_dev_3))
+# print("Accuracy DEV with k=5", accuracy_score(y_dev, y_pred_dev_5))
+# print("Accuracy DEV with k=7", accuracy_score(y_dev, y_pred_dev_7))
+print("Accuracy DEV with k=9", accuracy_score(y_dev, y_pred_dev_9))
+# print("Accuracy DEV with k=11", accuracy_score(y_dev, y_pred_dev_11))
  
 
-# # Print F1 score
-# print("F1-Score with k=1", f1_score(y_test, y_pred_1))
-# print("F1-Score with k=1", f1_score(y_test, y_pred_3))
-# print("F1-Score with k=5", f1_score(y_test, y_pred_5))
-# print("F1-Score with k=7", f1_score(y_test, y_pred_7))
-# print("F1-Score with k=9", f1_score(y_test, y_pred_9))
+# Print F1 score dev
+# print("F1-Score DEV with k=1", f1_score(y_dev, y_pred_dev_1))
+# print("F1-Score DEV with k=3", f1_score(y_dev, y_pred_dev_3))
+# print("F1-Score DEV with k=5", f1_score(y_dev, y_pred_dev_5))
+# print("F1-Score DEV with k=7", f1_score(y_dev, y_pred_dev_7))
+print("F1-Score DEV with k=9", f1_score(y_dev, y_pred_dev_9))
+# print("F1-Score DEV with k=11", f1_score(y_dev, y_pred_dev_11))
+
+
+# Print accuracy test
+# print("Accuracy TEST with k=1", accuracy_score(y_test, y_pred_test_1))
+# print("Accuracy TEST with k=3", accuracy_score(y_test, y_pred_test_3))
+# print("Accuracy TEST with k=5", accuracy_score(y_test, y_pred_test_5))
+# print("Accuracy TEST with k=7", accuracy_score(y_test, y_pred_test_7))
+print("Accuracy TEST with k=9", accuracy_score(y_test, y_pred_test_9))
+# print("Accuracy TEST with k=11", accuracy_score(y_test, y_pred_test_11))
+ 
+
+# Print F1 score test
+# print("F1-Score TEST with k=1", f1_score(y_test, y_pred_test_1))
+# print("F1-Score TEST with k=3", f1_score(y_test, y_pred_test_3))
+# print("F1-Score TEST with k=5", f1_score(y_test, y_pred_test_5))
+# print("F1-Score TEST with k=7", f1_score(y_test, y_pred_test_7))
+print("F1-Score TEST with k=9", f1_score(y_test, y_pred_test_9))
+# print("F1-Score TEST with k=11", f1_score(y_test, y_pred_test_11))
+
+# Print AUC-ROC
+
+roc_auc_dev = roc_auc_score(y_dev, y_prob9_dev)
+roc_auc_test = roc_auc_score(y_test, y_prob9_test)
+
+print("AUC-ROC DEV:", roc_auc_dev)
+print("AUC-ROC TEST:", roc_auc_test)
+
+
+# # Plot the ROC curve
+# fpr_test, tpr_test, thresholds_test = roc_curve(y_test, y_prob9_test)
+# roc_auc_test = auc(fpr_test, tpr_test)
+
+# fpr_dev, tpr_dev, thresholds_dev = roc_curve(y_dev, y_prob9_dev)
+# roc_auc_dev = auc(fpr_dev, tpr_dev)
+
+# plt.figure()
+# plt.plot(fpr_dev, tpr_dev, color='darkorange', lw=2, label=f' Dev ROC curve (area = {roc_auc_dev:.2f})')
+# plt.plot(fpr_test, tpr_test, color='fuchsia', lw=2, label=f' Test ROC curve (area = {roc_auc_test:.2f})')
+# plt.plot([0, 1], [0, 1], color='black', lw=2, linestyle='--')
+# plt.xlim([0.0, 1.0])
+# plt.ylim([0.0, 1.05])
+# plt.xlabel('False Positive Rate')
+# plt.ylabel('True Positive Rate')
+# plt.title('KNN AUC-ROC with Animacy')
+# plt.legend(loc="lower right")
+# plt.show()
