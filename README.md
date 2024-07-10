56482181ccf154d2f03a13d16c15840668731844
ukhovs <ukhovsergeyy@gmail.com>
2024-07-10 22:17:25 +0200

    Updated README + knn.py commented out

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
-Shawn:
+## Project Setup
 
+> :warning: **IMPORTANT: Please adjust the file paths/file names in the `BERT.py (Lines 10-13)`, `kmeans.py (Lines 28-31)` and `RandomForest.py (Lines 115-118)` files to the appropriate paths/names on your local machine before running the program.`KNN`: follow the multistring instructions in knn.py to get the results for a desired model**
+
+```python
+python3 -m venv semantic-plausibility
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+1. Python version:
+   - Python (3.12.3)
+2. Overview:
+
+Five models in total have been implemented: K-means, Random Forest, KNN, FNN, BERT.
+
+**K-means**
+**Random Forest**
+**KNN**
+Hyperparameters: k=9, distance='manhattan'. Other k numbers and distances have been tested, but this pair generally gives the best results.
+
+The model's settings are preselected to showing the results for the original data and separated animacy features added, as it has the highest AUC-ROC score. If you want to see the results for other options, please follow the multistring instructions in knn.py.
+**FNN**
+
+**BERT**
+Tune the hyperparameters in lines 141-144
+
+3. To get the models work, simply run the files.
 
 ## Evaluation Results on Augmented (Original) Data
 
@@ -9,7 +35,7 @@ With separated animacy feature:
 
 - Dev data F1 Score: 0.5784 ; Accuracy: 0.6046 ; AUC-ROC score: 0.6046
 - Test data F1 Score: 0.5068 ; Accuracy: 0.5244 ; AUC-ROC score: 0.5243
-  
+
 With concatenated animacy feature:
 
 - Dev data F1 Score: 0.5764 ; Accuracy: 0.6013 ; AUC-ROC score: 0.6013
@@ -19,7 +45,6 @@ Without animacy feature:
 
 - Dev data F1 Score: 0.5764 ; Accuracy: 0.6013 ; AUC-ROC score: 0.6013
 - Test data F1 Score: 0.5390 ; Accuracy: 0.5375 ; AUC-ROC score: 0.5375
-  
 
 **Random Forest:**
 
@@ -27,7 +52,7 @@ With separated animacy feature:
 
 - Dev data F1 Score: 0.6222 (0.6433); Accuracy: 0.6111 (0.6340); AUC-ROC score: 0.6583 (0.6728)
 - Test data F1 Score: 0.6144 (0.5753); Accuracy: 0.6156 (0.5961); AUC-ROC score: 0.6634 (0.6532)
-  
+
 With combined animacy feature:
 
 - Dev data F1 Score: 0.6076 (0.6026); Accuracy: 0.5948 (0.6078); AUC-ROC score: 0.6523 (0.6667)
@@ -37,10 +62,25 @@ Without animacy feature:
 
 - Dev data F1 Score: 0.6149 (0.5933); Accuracy: 0.6111 (0.6013); AUC-ROC score: 0.6670 (0.6533)
 - Test data F1 Score: 0.6 (0.6312); Accuracy: 0.5961 (0.6384); AUC-ROC score: 0.6275 (0.6833)
-  
+
+**KNN**
+
+With separated animacy feature:
+
+- Dev data F1 Score: 0.6075 (0.6105); Accuracy: 0.5947 (0.5915); AUC-ROC score: 0.6234 (0.6065)
+- Test data F1 Score: 0.5855 (0.5825); Accuracy: 0.5895 (0.5798); AUC-ROC score: 0.6271 (0.6347)
+
+With concatenated animacy feature:
+
+- Dev data F1 Score: 0.5904 (0.6172); Accuracy: 0.5784 (0.5947); AUC-ROC score: 0.6006 (0.5926)
+- Test data F1 Score: 0.5909 (0.5863); Accuracy: 0.5895 (0.5863); AUC-ROC score: 0.6341 (0.6282)
+
+Without animacy feature:
+
+- Dev data F1 Score: 0.5677 (0.5816); Accuracy: 0.5620 (0.5816); AUC-ROC score: 0.5835 (0.6068)
+- Test data F1 Score: 0.5762 (0.5647); Accuracy: 0.5928 (0.5732); AUC-ROC score: 0.6188 (0.5940)
 
 **BERT:**
-**Tune the hyperparameters in lines 141-144
 
 With separated animacy feature:
 
@@ -56,22 +96,3 @@ Without animacy feature:
 
 - Dev data F1 Score: 0.7355 (0.7538); Accuracy: 0.7311 (0.7344); AUC-ROC score: 0.7755 (0.8053)
 - Test data F1 Score: 0.7248 (0.7101); Accuracy: 0.7320 (0.7092) ; AUC-ROC score: 0.8106 (0.7796)
-
-  
-## Project Setup
-
-> :warning: **IMPORTANT: Please adjust the file paths/file names in the `BERT.py (Lines 10-13)`, `kmeans.py (Lines 28-31)` and `RandomForest.py (Lines 115-118)` files to the appropriate paths/names on your local machine before running the program.**
-
-```python
-python3 -m venv team_lab
-source team_lab/bin/activate
-requirement.txt
-```
-1. Python version:
-   - Python (3.12.3)
-     
-2. Overview:
-   
-3. To get the models work, simply run the files.
-
-
--- knn.py
+++ knn.py
@@ -11,15 +11,17 @@ from sklearn.metrics import roc_auc_score, roc_curve, auc
 
 def get_sentence_embeddings(text_list, batch_size=32):
     """
+    Generates sentence embeddings for a list of texts using a BERT model.
 
-    Normalizes word counts in the dictionary values to respective probability distributions. Keys are the same.
+    Parameters:
+    text_list (list): List of strings to generate embeddings for.
+    batch_size (int): Number of texts to process in a batch.
 
-    :param word_counts: dict
-    :return: dict
-
+    Returns:
+    torch.Tensor: Tensor containing sentence embeddings.
     """
     all_embeddings = []
-
+    
     # Process each sublist separately
     for i in range(0, len(text_list), batch_size):
         batch = text_list[i:i + batch_size]
@@ -39,222 +41,242 @@ def get_sentence_embeddings(text_list, batch_size=32):
 
 
 def get_strings(dataframe):
+    """
+    Extracts text data from a dataframe.
+
+    Parameters:
+    dataframe (pd.DataFrame): DataFrame containing text data.
+
+    Returns:
+    list: List of text strings.
+    """
     strings_for_bert = []
     for index, sentence in enumerate(dataframe['text']):
         strings_for_bert.append (sentence)
     return strings_for_bert
 
 
-#load files
+def load_data(file_path):
+    """
+    Loads data from a CSV file. The function is used to read added animacy features which will be
+    consecutively added to the BERT embeddings. 
+
+    Parameters:
+    file_path (str): Path to the CSV file.
+
+    Returns:
+    tuple: A tuple containing lists of texts, features, and labels.
+    """
+    data = pd.read_csv(file_path, header=None)
+    texts = data[1].tolist()
+    features = data[[2, 3]].values
+    labels = data[0].values
+    return texts, features, labels
+
+
+def weighted_features_separated(features):
+    """
+    Multiples separated animacy features by a constant.
+
+    Parameters:
+    features (list): list of animacy features.
+
+    Retures:
+    new_features (list): each element of a list is multiplied by a constant.
+    """
+    animacy_weight = 20
+    new_features = []
+    for i in features:
+        new_features.append(i*animacy_weight)
+    return new_features
+
+
+def weighted_features_combined(features):
+    """
+    Multiples combined animacy features by a constant.
+
+    Parameters:
+    features (list): list of animacy features.
+
+    Retures:
+    new_features (list): each element of a list is multiplied by a constant.
+    """
+    animacy_weight = 20
+    new_features = []
+    for i in features:
+        new_features.append([sum(i)*animacy_weight])
+    return new_features
+
+
+#Load files
 custom_headers = ['label','text','anim_s','anim_o']
 train_data = pd.read_csv('train.csv',skiprows=1, header=None, names=custom_headers)
 test_data = pd.read_csv('test.csv', skiprows=1, header=None, names=custom_headers)
 dev_data = pd.read_csv('dev.csv', skiprows=1, header=None, names=custom_headers)
 train_augmented_data = pd.read_csv('data_augmented.csv',skiprows=1, header=None, names=custom_headers)
 
-# get strings from data to make BERT embeddings
+# Get strings from data to make BERT embeddings
 train_strings = get_strings(train_data)
 test_strings = get_strings(test_data)
 dev_strings = get_strings(dev_data)
 train_augmented_strings = get_strings(train_augmented_data)
 
+# BERT
 
-print(train_strings)
-# # BERT
-# tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-# bert_model = BertModel.from_pretrained('bert-base-uncased')
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+bert_model = BertModel.from_pretrained('bert-base-uncased')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = BertModel.from_pretrained('bert-base-uncased').to(device)
 
-# model = BertModel.from_pretrained('bert-base-uncased').to(device)
-
-# # Get train embeddings
+# Get train embeddings
 # train_augmented_sentence_embeddings = get_sentence_embeddings(train_augmented_strings)
-# # train_sentence_embeddings = get_sentence_embeddings(train_strings)
-# # print("Train sentence embeddings shape:", train_sentence_embeddings.shape)
-# # torch.save(train_augmented_sentence_embeddings, "train_augmented_sentence_embeddings.pt")
+# train_sentence_embeddings = get_sentence_embeddings(train_strings)
+# print("Train sentence embeddings shape:", train_sentence_embeddings.shape)
+# torch.save(train_augmented_sentence_embeddings, "train_augmented_sentence_embeddings.pt")
+
+"""
+The lines commented above were used to get BERT train embeddings and save them. Depending on whether you are running
+a model with augmented train set or not, select one of the loaded embeddings below and comment the other one.
+"""
 # loaded_embeddings = torch.load("train_augmented_sentence_embeddings.pt")
-# # print(train_sentence_embeddings)
+loaded_embeddings = torch.load("train_sentence_embeddings.pt")
 
-# # # Get test embeddings
-# test_sentence_embeddings = get_sentence_embeddings(test_strings)
-# dev_sentence_embeddings = get_sentence_embeddings(dev_strings)
-# # X_train = train_sentence_embeddings
-# # y_train = list(train_data['label'])
+# Get test and dev embeddings
+test_sentence_embeddings = get_sentence_embeddings(test_strings)
+dev_sentence_embeddings = get_sentence_embeddings(dev_strings)
 
-# # X_test = test_sentence_embeddings
-# # y_test = list(test_data['label'])
+# Retrieve data from labeled sets used for checking whether animacy improves the model's performance or not
+train_augmented_texts, train_augmented_features, train_augmented_labels = load_data('data_augmented_a.csv')
+train_texts, train_features, train_labels = load_data('train_labeled.csv')
+test_texts, test_features, test_labels = load_data('test_labeled.csv')
+dev_texts, dev_features, dev_labels = load_data('dev_labeled.csv')
 
-# # rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
-# # rf_classifier.fit(X_train, y_train)
+# Weight animacy features combined for each dataset
+new_augmented_train_features_combined = weighted_features_combined(train_augmented_features)
+new_train_features_combined = weighted_features_combined(train_features)
+new_test_features_combined = weighted_features_combined(test_features)
+new_dev_features_combined = weighted_features_combined(dev_features)
 
-# # # Predict on the test set
-# # y_pred = rf_classifier.predict(X_test)
+# Weight animacy features separated for each dataset
+new_augmented_train_features_separated = weighted_features_separated(train_augmented_features)
+new_train_features_separated = weighted_features_separated(train_features)
+new_test_features_separated = weighted_features_separated(test_features)
+new_dev_features_separated = weighted_features_separated(dev_features)
 
-# # # Evaluate the model
-# # accuracy = accuracy_score(y_test, y_pred)
-# # print(f"Accuracy: {accuracy}")
+"""
+Concatenate BERT embeddings with combined animacy features. If you run the model on the augmented train data, make sure
+that you use augmented_train_features_combined variable, and train_features_combined is commented, and vice versa, if
+you use the original train data.
+"""
 
+# augmented_train_features_combined = np.concatenate((loaded_embeddings, new_augmented_train_features_combined),axis=1)
+train_features_combined = np.concatenate((loaded_embeddings, new_train_features_combined),axis=1)
+test_features_combined = np.concatenate((test_sentence_embeddings, new_test_features_combined), axis=1)
+dev_features_combined = np.concatenate((dev_sentence_embeddings, new_dev_features_combined), axis=1)
 
-# def load_data(file_path):
-#     data = pd.read_csv(file_path, header=None)
-#     texts = data[1].tolist()
-#     features = data[[2, 3]].values
-#     labels = data[0].values
-#     return texts, features, labels
+"""
+Concatenate BERT embeddings with separated animacy features. If you run the model on the augmented train data, make sure
+that you use augmented_train_features_separated variable, and train_features_separated is commented, and vice versa, if
+you use the original train data.
+"""
 
-# train_texts, train_features, train_labels = load_data('train_labeled.csv')
-# test_texts, test_features, test_labels = load_data('test_labeled.csv')
-# dev_texts, dev_features, dev_labels = load_data('dev_labeled.csv')
+# augmented_train_features_separated = np.concatenate((loaded_embeddings, new_augmented_train_features_separated), axis=1)
+train_features_separated = np.concatenate((loaded_embeddings, new_train_features_separated), axis=1)
+test_features_separated = np.concatenate((test_sentence_embeddings, new_test_features_separated), axis=1)
+dev_features_separated = np.concatenate((dev_sentence_embeddings, new_dev_features_separated), axis=1)
 
+# KNN Classifier 
 
-# animacy_weight = 20
-# new_train_features = []
-# for i in train_features:
-#     new_train_features.append([sum(i)*animacy_weight])
-#     # new_train_features.append([sum(i)])
-# # print(new_train_features)
+knn9 = KNeighborsClassifier(n_neighbors=9,metric='manhattan')
 
-# new_test_features = []
-# for i in test_features:
-#     new_test_features.append([sum(i)*animacy_weight])
-#     # new_test_features.append([sum(i)])
-# # print(new_test_features)
+"""
+Choosing the right X_train. 
 
-# new_dev_features = []
-# for i in dev_features:
-#     new_dev_features.append([sum(i)*animacy_weight])
-#     # new_dev_features.append([sum(i)])
+loaded_embeddings - not checking for animacy, regardless of whether the train set is augmented or not.
+train_features_combined - checking for animacy with combined animacy features, original train set.
+train_features_separated - checking for animacy with separated animacy features, original train set.
+augmented_train_features_combined - checking for animacy with combined animacy features, augmented train set.
+augmented_train_features_separated - checking for animacy with separated animacy features, augmented train set.
 
-
-
-# # Adding animacy features to BERT embeddings
-# # train_features_combined = np.concatenate((train_sentence_embeddings, new_train_features),axis=1)
-# # test_features_combined = np.concatenate((test_sentence_embeddings, new_test_features), axis=1)
-# # dev_features_combined = np.concatenate((dev_sentence_embeddings, new_dev_features), axis=1)
-# # print(test_features_combined)
-
-
-# # KNN Classifier 
-# knn1 = KNeighborsClassifier(n_neighbors=1,metric='manhattan')
-# knn3 = KNeighborsClassifier(n_neighbors=3,metric='manhattan')
-# knn5 = KNeighborsClassifier(n_neighbors=5,metric='manhattan')
-# knn7 = KNeighborsClassifier(n_neighbors=7,metric='manhattan')
-# knn9 = KNeighborsClassifier(n_neighbors=9,metric='manhattan')
-# knn11 = KNeighborsClassifier(n_neighbors=11,metric='manhattan')
+Once you have chosen the correct one, comment all other options.
+"""
 
 # X_train = loaded_embeddings
+# X_train = train_features_combined
+X_train = train_features_separated
+# X_train = augmented_train_features_separated
+# X_train = augmented_train_features_combined
+
+"""Choose y_train in the same way."""
+
+y_train = list(train_data['label'])
 # y_train = list(train_augmented_data['label'])
-# # X_train = train_sentence_embeddings
-# # X_train = train_features_combined
-# # y_train = list(train_data['label'])
 
+"""Similarly to choosing X_train, choose the appropriate X_test and make sure others are commented out"""
 # X_test = test_sentence_embeddings
-# # X_test = test_features_combined
-# y_test = list(test_data['label'])
+# X_test = test_features_combined
+X_test = test_features_separated
+y_test = list(test_data['label'])
 
+"""Similarly to choosing X_test, choose the appropriate X_dev and make sure others are commented out"""
 # X_dev = dev_sentence_embeddings
-# # X_dev = dev_features_combined
-# y_dev = list(dev_data['label'])
+# X_dev = dev_features_combined
+X_dev = dev_features_separated
+y_dev = list(dev_data['label'])
 
-# # Train KNN
+# Train KNN
 
-# knn1.fit(X_train, y_train)
-# knn3.fit(X_train, y_train)
-# knn5.fit(X_train, y_train)
-# knn7.fit(X_train, y_train)
-# knn9.fit(X_train, y_train)
-# knn11.fit(X_train, y_train)
+knn9.fit(X_train, y_train)
 
+# Predict 
 
-# # Predict 
+y_pred_dev_9 = knn9.predict(X_dev)
+y_pred_test_9 = knn9.predict(X_test)
 
-# y_pred_dev_1 = knn1.predict(X_dev)
-# y_pred_dev_3 = knn3.predict(X_dev)
-# y_pred_dev_5 = knn5.predict(X_dev)
-# y_pred_dev_7 = knn7.predict(X_dev)
-# y_pred_dev_9 = knn9.predict(X_dev)
-# y_pred_dev_11 = knn11.predict(X_dev)
+# Predict using probabilities (used for AUC-ROC evaluation)
+y_prob9_dev = knn9.predict_proba(X_dev)[:, 1]
+y_prob9_test = knn9.predict_proba(X_test)[:, 1]
 
-# y_prob1_dev = knn1.predict_proba(X_dev)[:, 1]
-# y_prob3_dev = knn3.predict_proba(X_dev)[:, 1]
-# y_prob5_dev = knn5.predict_proba(X_dev)[:, 1]
-# y_prob7_dev = knn7.predict_proba(X_dev)[:, 1]
-# y_prob9_dev = knn9.predict_proba(X_dev)[:, 1]
-# y_prob11_dev = knn11.predict_proba(X_dev)[:, 1]
+# Print accuracy dev
 
-# y_pred_test_1 = knn1.predict(X_test)
-# y_pred_test_3 = knn3.predict(X_test)
-# y_pred_test_5 = knn5.predict(X_test)
-# y_pred_test_7 = knn7.predict(X_test)
-# y_pred_test_9 = knn9.predict(X_test)
-# y_pred_test_11 = knn11.predict(X_test)
-
-# y_prob1_test = knn1.predict_proba(X_test)[:, 1]
-# y_prob3_test = knn3.predict_proba(X_test)[:, 1]
-# y_prob5_test = knn5.predict_proba(X_test)[:, 1]
-# y_prob7_test = knn7.predict_proba(X_test)[:, 1]
-# y_prob9_test = knn9.predict_proba(X_test)[:, 1]
-# y_prob11_test = knn11.predict_proba(X_test)[:, 1]
-
-# # Print accuracy dev
-# print("Accuracy DEV with k=1", accuracy_score(y_dev, y_pred_dev_1))
-# print("Accuracy DEV with k=3", accuracy_score(y_dev, y_pred_dev_3))
-# print("Accuracy DEV with k=5", accuracy_score(y_dev, y_pred_dev_5))
-# print("Accuracy DEV with k=7", accuracy_score(y_dev, y_pred_dev_7))
-# print("Accuracy DEV with k=9", accuracy_score(y_dev, y_pred_dev_9))
-# print("Accuracy DEV with k=11", accuracy_score(y_dev, y_pred_dev_11))
+print("Accuracy DEV with k=9", accuracy_score(y_dev, y_pred_dev_9))
  
+# Print F1 score dev
 
-# # Print F1 score dev
-# print("F1-Score DEV with k=1", f1_score(y_dev, y_pred_dev_1))
-# print("F1-Score DEV with k=3", f1_score(y_dev, y_pred_dev_3))
-# print("F1-Score DEV with k=5", f1_score(y_dev, y_pred_dev_5))
-# print("F1-Score DEV with k=7", f1_score(y_dev, y_pred_dev_7))
-# print("F1-Score DEV with k=9", f1_score(y_dev, y_pred_dev_9))
-# print("F1-Score DEV with k=11", f1_score(y_dev, y_pred_dev_11))
+print("F1-Score DEV with k=9", f1_score(y_dev, y_pred_dev_9))
 
+# Print accuracy test
 
-# # Print accuracy test
-# print("Accuracy TEST with k=1", accuracy_score(y_test, y_pred_test_1))
-# print("Accuracy TEST with k=3", accuracy_score(y_test, y_pred_test_3))
-# print("Accuracy TEST with k=5", accuracy_score(y_test, y_pred_test_5))
-# print("Accuracy TEST with k=7", accuracy_score(y_test, y_pred_test_7))
-# print("Accuracy TEST with k=9", accuracy_score(y_test, y_pred_test_9))
-# print("Accuracy TEST with k=11", accuracy_score(y_test, y_pred_test_11))
- 
+print("Accuracy TEST with k=9", accuracy_score(y_test, y_pred_test_9))
 
-# # Print F1 score test
-# print("F1-Score TEST with k=1", f1_score(y_test, y_pred_test_1))
-# print("F1-Score TEST with k=3", f1_score(y_test, y_pred_test_3))
-# print("F1-Score TEST with k=5", f1_score(y_test, y_pred_test_5))
-# print("F1-Score TEST with k=7", f1_score(y_test, y_pred_test_7))
-# print("F1-Score TEST with k=9", f1_score(y_test, y_pred_test_9))
-# print("F1-Score TEST with k=11", f1_score(y_test, y_pred_test_11))
+# Print F1 score test
 
-# # Print AUC-ROC
+print("F1-Score TEST with k=9", f1_score(y_test, y_pred_test_9))
 
-# roc_auc_dev = roc_auc_score(y_dev, y_prob9_dev)
-# roc_auc_test = roc_auc_score(y_test, y_prob9_test)
+# Print AUC-ROC
 
-# print("AUC-ROC DEV:", roc_auc_dev)
-# print("AUC-ROC TEST:", roc_auc_test)
+roc_auc_dev = roc_auc_score(y_dev, y_prob9_dev)
+roc_auc_test = roc_auc_score(y_test, y_prob9_test)
 
+print("AUC-ROC DEV:", roc_auc_dev)
+print("AUC-ROC TEST:", roc_auc_test)
 
-# # # Plot the ROC curve
-# # fpr_test, tpr_test, thresholds_test = roc_curve(y_test, y_prob9_test)
-# # roc_auc_test = auc(fpr_test, tpr_test)
+"""Uncomment the below to plot the AUC-ROC"""
+# # Plot the ROC curve
+# fpr_test, tpr_test, thresholds_test = roc_curve(y_test, y_prob9_test)
+# roc_auc_test = auc(fpr_test, tpr_test)
 
-# # fpr_dev, tpr_dev, thresholds_dev = roc_curve(y_dev, y_prob9_dev)
-# # roc_auc_dev = auc(fpr_dev, tpr_dev)
+# fpr_dev, tpr_dev, thresholds_dev = roc_curve(y_dev, y_prob9_dev)
+# roc_auc_dev = auc(fpr_dev, tpr_dev)
 
-# # plt.figure()
-# # plt.plot(fpr_dev, tpr_dev, color='darkorange', lw=2, label=f' Dev ROC curve (area = {roc_auc_dev:.2f})')
-# # plt.plot(fpr_test, tpr_test, color='fuchsia', lw=2, label=f' Test ROC curve (area = {roc_auc_test:.2f})')
-# # plt.plot([0, 1], [0, 1], color='black', lw=2, linestyle='--')
-# # plt.xlim([0.0, 1.0])
-# # plt.ylim([0.0, 1.05])
-# # plt.xlabel('False Positive Rate')
-# # plt.ylabel('True Positive Rate')
-# # plt.title('KNN AUC-ROC with Animacy')
-# # plt.legend(loc="lower right")
-# # plt.show()
+# plt.figure()
+# plt.plot(fpr_dev, tpr_dev, color='darkorange', lw=2, label=f' Dev ROC curve (area = {roc_auc_dev:.2f})')
+# plt.plot(fpr_test, tpr_test, color='fuchsia', lw=2, label=f' Test ROC curve (area = {roc_auc_test:.2f})')
+# plt.plot([0, 1], [0, 1], color='black', lw=2, linestyle='--')
+# plt.xlim([0.0, 1.0])
+# plt.ylim([0.0, 1.05])
+# plt.xlabel('False Positive Rate')
+# plt.ylabel('True Positive Rate')
+# plt.title('KNN AUC-ROC')
+# plt.legend(loc="lower right")
+# plt.show()
